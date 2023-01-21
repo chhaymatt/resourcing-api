060303a1633a141c5b603d810de8bbba06838641
Matthew Chhay <chhaymatt@gmail.com>
2023-01-21 17:18:45 +1030

    Move HTTP related to controller instead of service

--- src/main/java/com/matthewchhay/resourcingapi/jobs/JobService.java
+++ src/main/java/com/matthewchhay/resourcingapi/jobs/JobService.java
@@ -3,21 +3,16 @@ package com.matthewchhay.resourcingapi.jobs;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 import com.matthewchhay.resourcingapi.temps.Temp;
-import com.matthewchhay.resourcingapi.temps.TempService;
 
 @Service
 @Transactional
 public class JobService {
     @Autowired
     private JobRepository repository;
-
-    @Autowired
-    private TempService tempService;
 
     public String hello() {
         return "Hello World from Job";
@@ -29,28 +24,17 @@ public class JobService {
 
     public Optional<Job> findOne(Long jobId) {
         Optional<Job> maybeJob = this.repository.findById(jobId);
-        if (maybeJob.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "no job");
-        }
         return maybeJob;
     }
 
-    public Job create(JobCreateDTO data) {
-        Temp temp = null;
-        if (data.temp != null) {
-            temp = tempService.findOne(data.temp).get();
-        }
-        String cleanedName = data.name.trim();
-        Job newJob = new Job(cleanedName, data.startDate, data.endDate, temp);
+    public Job create(JobCreateDTO data, Temp temp) {
+        Job newJob = new Job(data.name.trim(), data.startDate, data.endDate, temp);
         return this.repository.save(newJob);
     }
 
-    public Job update(Long jobId, JobUpdateDTO data) {
-        Job job = this.findOne(jobId).get();
-
+    public Job update(Long jobId, JobUpdateDTO data, Job job, Temp temp) {
         if (data.name != null) {
-            String cleanedName = data.name.trim();
-            job.setName(cleanedName);
+            job.setName(data.name.trim());
         }
 
         if (data.startDate != null) {
@@ -61,9 +45,8 @@ public class JobService {
             job.setEndDate(data.endDate);
         }
 
-        if (data.temp != null) {
-            Temp foundTemp = tempService.findOne(data.temp).get();
-            job.setTemp(foundTemp);
+        if (temp != null) {
+            job.setTemp(temp);
         }
 
         return this.repository.save(job);
--- src/main/java/com/matthewchhay/resourcingapi/temps/TempService.java
+++ src/main/java/com/matthewchhay/resourcingapi/temps/TempService.java
@@ -2,12 +2,9 @@ package com.matthewchhay.resourcingapi.temps;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.transaction.Transactional;
 
@@ -23,52 +20,31 @@ public class TempService {
 
     public Optional<Temp> findOne(Long TempId) {
         Optional<Temp> maybeTemp = this.repository.findById(TempId);
-        if (maybeTemp.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "no temp");
-        }
         return maybeTemp;
     }
 
-    public Temp create(TempCreateDTO data) {
+    public Temp create(TempCreateDTO data, List<Temp> temps) {
         String cleanedFirstName = data.firstName.trim();
         String cleanedLastName = data.lastName.trim();
-        List<Temp> temps = null;
-
-        // Find each Temp based on temps = [tempId_1, tempId_2, ...]
-        if (data.temps != null) {
-            temps = data.temps.stream().map(tempId -> this.findOne(tempId).get()).collect(Collectors.toList());
-        }
-
         Temp newTemp = new Temp(cleanedFirstName, cleanedLastName, temps);
-
         return this.repository.save(newTemp);
     }
 
-    public Temp update(Long TempId, TempUpdateDTO data) {
-        Temp Temp = this.findOne(TempId).get();
-
+    public Temp update(Long TempId, TempUpdateDTO data, Temp temp, List<Temp> temps) {
         if (data.firstName != null) {
             String cleanedFirstName = data.firstName.trim();
-            Temp.setFirstName(cleanedFirstName);
+            temp.setFirstName(cleanedFirstName);
         }
 
         if (data.lastName != null) {
             String cleanedLastName = data.lastName.trim();
-            Temp.setLastName(cleanedLastName);
+            temp.setLastName(cleanedLastName);
         }
 
-        if (data.temps != null) {
-            Temp.setTemps(data.temps.stream().map(tempId -> this.findOne(tempId).get()).collect(Collectors.toList()));
+        if (temps != null) {
+            temp.setTemps(temps);
         }
 
-        // Throw error if temp id equals the one from the payload
-        for (Long tempId : data.temps) {
-            if (tempId == Temp.getId()) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                        "Cannot assign temp id to itself");
-            }
-        }
-
-        return this.repository.save(Temp);
+        return this.repository.save(temp);
     }
 }
